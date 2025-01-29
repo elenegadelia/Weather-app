7556eb654471e1f6f29978d05ef17c100a430ef2
elenegadelia <elenegadelia@gmail.com>
2025-01-29 10:38:28 +0400

    Create weather block components layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import WeatherBlock from "./components/WeatherBlock.vue";
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="page">
+    <p>Weather Forecast</p>
+    <div class="weather-forecast">
+      <WeatherBlock v-for="index in 7" :key="index" />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.page {
+  width: 100%;
+
+  padding: 10px 80px;
+}
+
+p {
+  display: flex;
+  justify-content: center;
+  font-size: 40px;
+  margin: 10px auto;
+}
+
+.weather-forecast {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 18px;
+
+  width: 100%;
+}
+</style>
---
+++ src/components/WeatherBlock.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="weather"></div>
+</template>
+
+<style scoped>
+.weather {
+  height: 184px;
+  width: 342px;
+
+  background-image: url("../assets/weather-block.svg");
+  background-repeat: no-repeat;
+  background-position: 0%;
+}
+</style>
